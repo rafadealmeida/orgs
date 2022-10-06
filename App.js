b26d02b9b0397f7ebb113d85d307dde3fff02109
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-10-05 22:24:24 -0300

    Usando SplashScreen

--- App.js
+++ App.js
@@ -1,24 +1,42 @@
-
+import { useCallback, useEffect } from 'react';
 import { StyleSheet,StatusBar,SafeAreaView, View } from 'react-native';
 import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from "@expo-google-fonts/montserrat";
 import Cesta from './src/telas/cesta/cesta';
 import {CestaContext,cesta} from './src/mocks/cesta'
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
-  const [fonteCarregada] = useFonts({
-    "MontserratRegular":Montserrat_400Regular,
-    'MontserratBold': Montserrat_700Bold,
+  const [fontsLoaded] = useFonts({
+    MontserratRegular: Montserrat_400Regular,
+    MontserratBold: Montserrat_700Bold,
   });
 
-  if(!fonteCarregada){
-    return <View/>
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
   }
 
-
   return (
+    
     <CestaContext.Provider value={cesta} >
-
-    <SafeAreaView>
+      
+    <SafeAreaView onLayout={onLayoutRootView} >
       <StatusBar/>
       <Cesta/>
     </SafeAreaView>
--- src/mocks/cesta.js
+++ src/mocks/cesta.js
@@ -1,5 +1,10 @@
-import logo from '../../assets/logo.png'
-import { createContext } from 'react'
+import logo from '../../assets/logo.png';
+import { createContext } from 'react';
+import Tomate from '../../assets/frutas/Tomate.png';
+import Brocolis from '../../assets/frutas/Brócolis.png';
+import Pepino from '../../assets/frutas/Pepino.png';
+import Abobora from '../../assets/frutas/Abóbora.png';
+import Batata from '../../assets/frutas/Batata.png'
 
 
 export const CestaContext = createContext()
@@ -13,6 +18,32 @@ export const cesta = {
   logoFazenda: logo,
   nomeFazenda:"Jenny Jack Farm",
   descricao: 'Uma cesta com produtos selecionados cuidadosamentew da fazenda diretamente para a sua cozinha',
-  preco: 'R$ 40,00'
- }
+  preco: 'R$ 40,00',
+  botao: "Comprar",
+ },
+ itens: {
+  titulo: "Itens da cesta",
+  lista: [
+    {
+      nome: "Tomate",
+      imagem: Tomate,
+    },
+    {
+      nome: "Brócolis",
+      imagem: Brocolis,
+    },
+    {
+      nome: "Batata",
+      imagem: Batata,
+    },
+    {
+      nome: "Pepino",
+      imagem: Pepino,
+    },
+    {
+      nome: "Abóbora",
+      imagem: Abobora,
+    }
+  ]
+}
 }
